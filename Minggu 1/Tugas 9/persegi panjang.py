72a1f5771a9f765f0420967dc03185d40c336356
EmiliaSumbawa <115346687+EmiliaSumbawa@users.noreply.github.com>
2022-10-09 17:37:30 +0800

    Add files via upload

---
+++ Minggu 1/Tugas 9/persegi panjang.py
@@ -0,0 +1,7 @@
+print('Menghitung keliling dan luas persegi panjang \n=============================================')
+panjang=int(input('Masukkan panjang persegi panjang: '))
+lebar=int(input('Masukkan Lebar persegi panjang: '))
+luas=panjang*lebar
+keliling=2*(panjang*lebar)
+print('Luas persegi panjang adalah'+str(luas))
+print('Keliling persegi panjang adalah'+str(keliling))
