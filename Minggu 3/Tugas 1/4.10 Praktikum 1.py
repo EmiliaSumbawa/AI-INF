995a4dc4a859aa5f10fb386237dab9b0de4e1589
EmiliaSumbawa <115346687+EmiliaSumbawa@users.noreply.github.com>
2022-10-23 13:24:13 +0700

    Add files via upload

---
+++ Minggu 3/Tugas 1/4.10 Praktikum 1.py
@@ -0,0 +1,15 @@
+#tugas 2
+h="PROGRAM MENGHITUNG PEMBELIAN"
+garis="-"*100
+print(h.center(100))
+print(garis)
+
+harga=12000
+print("Harga satuan\t\t: Rp. "+str(harga))  
+Jumlah_pembelian=input("Jumlah pembelian\t: ")
+discon=harga*0.2
+print("Discon\t\t\t: "+str(discon))
+harga_total=int(harga)*int(Jumlah_pembelian)-discon
+print("Harga Total\t\t: "+str(harga_total))
+print("-"*100)
+
---
+++ Minggu 3/Tugas 1/4.10 Praktikum 3.py
@@ -0,0 +1,17 @@
+import os
+os.system('cls')
+h="PROGRAM PENJUALAN BUKU"
+garis="-"*100
+print(h.center(100))
+print(garis)
+
+harga=50000
+harga_satuan=input("Harga satuan         : Rp. ")
+Jumlah_pembelian=input("Jumlah pembelian     : ")
+discon=harga*0.5
+print("Discon               : Rp. "+str(discon))
+harga_total=int(harga)*int(Jumlah_pembelian)-discon
+print("Harga Total          : "+str(harga_total))
+
+garis="-"*100
+print(garis)
